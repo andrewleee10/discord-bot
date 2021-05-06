d26e2e0631db3e5a0b9ba3dfc856482a95d5ffa6
Andrew Lee <andrewleee10@gmail.com>
2021-05-06 16:06:54 -0700

    play and stop commands

--- commands/stop.js
+++ commands/stop.js
@@ -1,7 +1,11 @@
 module.exports = {
   name: 'stop',
-  description: 'this is a stop command!',
-  execute(message, args) {
-    message.channel.send('Stopping...')
+  description: 'Stop the bot and leave the channel!',
+  async execute(message, args) {
+    const voiceChannel = message.member.voice.channel
+
+    if (!voiceChannel) return message.channel.send('You need to be in a voice channel to execute this command!')
+    await voiceChannel.leave()
+    await message.channel.send('Leaving channel :wave:')
   }
 }
